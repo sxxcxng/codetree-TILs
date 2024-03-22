e22309918d88034b2c711ef19b868bafaa60a96e
soobin <79037489+sxxcxng@users.noreply.github.com>
2024-03-22 15:24:40 +0900

    Title: 모이자, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240322/모이자/gather.py
+++ 240322/모이자/gather.py
@@ -1,18 +1,13 @@
 n = int(input())
 num = list(map(int, input().split()))
 
-
 minSum = 0
-
 for i in range(n):
     sum = 0
     for j in range(n):
         sum += num[j] * abs(i-j)
     if minSum == 0:
         minSum = sum
-    elif minSum > sum:
-        minSum = sum
-    else:
-        minSum = minSum
+    minSum = min(sum, minSum)
 
 print(minSum)
